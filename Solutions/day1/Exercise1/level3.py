0be71fc6ef02d66abccd5dd4b717723f97a8fb12
Tamagz <muhammadrizkypratamaa@gmail.com>
2023-03-17 03:50:10 +0700

    done day 1

---
+++ Solutions/day1/Exercise1/level3.py
@@ -0,0 +1,19 @@
+print(type(99))
+print(type(4.6))
+print(type(5.6))
+print(type(4-4j))
+print(type('Uhuyyyy'))
+print(type(True))
+print(type(['binatang','binatung','belatung','nangka']))
+print(type(('Belalang','Sapi','Monyet')))
+print(type({2,3,6,7}))
+print(type({'name':'sueb',
+            'umur':'70 tahun'}))
+#Number(Integer, Float, Complex),
+#String, Boolean, List, Tuple, Set and Dictionary.
+
+
+#Find an Euclidian distance between (2, 3) and (10, 8)
+
+print((10-2)**2+(8-3)**2/9)
+#i am giving up to square root this value 
